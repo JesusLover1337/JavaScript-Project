dc18ed1966bdfab2796d4eeeef7325663f67a4b0
JesusLover1337 <wilmer.lundberg@skola.taby.se>
2023-04-24 08:49:10 +0200

    qrewfd Co-authored-by: copoke <copoke@users.noreply.github.com> Co-authored-by: Bers1a <Bers1a@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let ctx = canvas.getContext("2d");
+ctx.fillstyle = "black";
+ctx.fillRect(50, canvas.height - 200, 450, 200);
+ctx.fillRect(700, canvas.height - 200, 450, 200);
+ctx.fillRect(500, canvas.height - 500, 96, 96);
